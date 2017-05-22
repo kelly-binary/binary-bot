117ba1955a191fc581e0e962b1cf7a1fd546d900
Amin Marashi <marashi.s.amin@gmail.com>
2017-05-22 16:19:56 +0800

    Use redux-saga

--- src/botPage/bot/Interface/index.js
+++ src/botPage/bot/Interface/index.js
@@ -1,4 +1,4 @@
-import TradeEngine from '../NewTradeEngine';
+import TradeEngine from '../SagaTradeEngine';
 import { noop, subtractFixed, createDetails } from '../tools';
 import TicksInterface from './TicksInterface';
 import ToolsInterface from './ToolsInterface';
