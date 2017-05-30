0ccd82e643290553bd008a72d7f6aaeadc71c644
Amin Marashi <marashi.s.amin@gmail.com>
2017-05-30 15:06:06 +0800

    Use testSaga in error unit test

--- src/botPage/bot/NewTradeEngine/sagas/error/index.spec.js
+++ src/botPage/bot/NewTradeEngine/sagas/error/index.spec.js
@@ -1,15 +1,18 @@
 import { observer as globalObserver } from 'binary-common-utils/lib/observer';
-import { expectSaga, testSaga } from 'redux-saga-test-plan';
+import { testSaga } from 'redux-saga-test-plan';
 import * as actions from '../../constants/actions';
 import error from './';
 
 describe('error saga', () => {
     const payload = Error('some error');
     it('should call globalObserver with the error', () => {
-        expectSaga(error, { type: 'SOMETHING_ERROR', payload })
+        testSaga(error, { type: 'SOMETHING_ERROR', payload })
+            .next()
             .call([globalObserver, globalObserver.emit], 'Error', payload)
+            .next()
             .put({ type: actions.ERROR_OCCURRED, error: true })
-            .run();
+            .next()
+            .isDone();
     });
     it('should do nothing in case of non-error type', () => {
         testSaga(error, { type: 'SOMETHING' }).next().isDone();
