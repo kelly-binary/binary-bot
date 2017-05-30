7cfb68a012acfbe708b1e65e9e2bd9a8cf1f92a2
Amin Marashi <marashi.s.amin@gmail.com>
2017-05-30 14:07:45 +0800

    Add watchBefore saga

---
+++ src/botPage/bot/NewTradeEngine/sagas/watch/watchBefore/index.spec.js
@@ -0,0 +1,43 @@
+import { testSaga } from 'redux-saga-test-plan';
+import * as states from '../../../constants/states';
+import * as actions from '../../../constants/actions';
+import * as selectors from '../../selectors';
+import watchBefore from './';
+
+describe('watching before', () => {
+    const lastTick = 123456;
+
+    it('should exit scope if neither PROPOSALS_READY nor STARTED', () => {
+        testSaga(watchBefore)
+            .next()
+            .select(selectors.stageSelector)
+            .next(states.STOPPED)
+            .put({ type: actions.EXIT_SCOPE })
+            .next()
+            .isDone();
+    });
+    it('should wait for NEW_TICK if PROPOSALS_READY', () => {
+        testSaga(watchBefore)
+            .next()
+            .select(selectors.stageSelector)
+            .next(states.PROPOSALS_READY)
+            .take(actions.NEW_TICK)
+            .next({ payload: { lastTick } })
+            .put({ type: actions.CONTINUE_SCOPE, payload: { timestamp: lastTick } })
+            .next()
+            .isDone();
+    });
+    it('should wait for NEW_TICK and RECEIVE_PROPOSALS if STARTED', () => {
+        testSaga(watchBefore)
+            .next()
+            .select(selectors.stageSelector)
+            .next(states.STARTED)
+            .take([actions.NEW_TICK, actions.RECEIVE_PROPOSALS])
+            .next({ type: actions.NEW_TICK, payload: { lastTick } })
+            .take([actions.NEW_TICK, actions.RECEIVE_PROPOSALS])
+            .next({ type: actions.RECEIVE_PROPOSALS })
+            .put({ type: actions.CONTINUE_SCOPE, payload: { timestamp: lastTick } })
+            .next()
+            .isDone();
+    });
+});
