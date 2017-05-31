e1f294cd07d6098955520aa1426b8b818d54f367
kelly <kelly@binary.com>
2017-05-31 15:15:47 +0800

    Add requestedProposals reducer

--- src/botPage/bot/NewTradeEngine/reducers/proposalInfo/requestedProposals/index.js
+++ src/botPage/bot/NewTradeEngine/reducers/proposalInfo/requestedProposals/index.js
@@ -1,3 +1,12 @@
-const requestProposals = () => {};
+import * as actions from '../../../constants/actions';
+
+const requestProposals = (state = {}, action) => {
+    switch (action.type) {
+        case actions.REQUEST_PROPOSAL:
+            return action.payload;
+        default:
+            return state;
+    }
+};
 
 export default requestProposals;
