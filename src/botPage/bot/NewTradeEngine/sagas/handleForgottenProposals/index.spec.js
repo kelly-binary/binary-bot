445b359aabd3819febb22bae69f590612b8479d2
Amin Marashi <marashi.s.amin@gmail.com>
2017-05-31 17:07:02 +0800

    Fix the handleForgottenProposals test

--- src/botPage/bot/NewTradeEngine/sagas/handleForgottenProposals/index.spec.js
+++ src/botPage/bot/NewTradeEngine/sagas/handleForgottenProposals/index.spec.js
@@ -3,6 +3,7 @@ import * as actions from '../../constants/actions';
 import handleForgottenProposals from './';
 
 const proposalID = '123';
+const payload = { [proposalID]: undefined };
 const api = {
     unsubscribeByID() {},
 };
@@ -11,10 +12,11 @@ describe('handleForgottenProposals saga', () => {
     it('should dispatch forget proposal', () => {
         testSaga(handleForgottenProposals, proposalID)
             .next()
-            .put({ type: actions.FORGET_PROPOSAL, payload: proposalID })
+            .put({ type: `UPDATE_${actions.FORGOTTEN_PROPOSAL}`, payload })
             .next()
             .call([api, api.unsubscribeByID], proposalID)
             .next()
+            .put({ type: `REMOVE_${actions.FORGOTTEN_PROPOSAL}`, payload: proposalID })
             .isDone();
     });
 });
