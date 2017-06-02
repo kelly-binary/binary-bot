bb536de3fe912695024ca496ac1a340709ae1bed
Amin Marashi <marashi.s.amin@gmail.com>
2017-06-02 17:13:50 +0800

    Fix reducerCreator

--- src/botPage/bot/NewTradeEngine/reducers/reducerCreators/object/index.js
+++ src/botPage/bot/NewTradeEngine/reducers/reducerCreators/object/index.js
@@ -3,10 +3,11 @@ export default function objectReducerCreator(actionType) {
         switch (action.type) {
             case `UPDATE_${actionType}`:
                 return { ...state, ...action.payload };
-            case `REMOVE_${actionType}`:
+            case `REMOVE_${actionType}`: {
                 const newState = { ...state };
                 delete newState[action.payload];
                 return newState;
+            }
             default:
                 return state;
         }
