b7c3ea0e6658aedac3c0fadb8ab5ef5c2956c9bc
Amin Marashi <marashi.s.amin@gmail.com>
2017-06-02 12:31:14 +0800

    Fix the proposal subscription test

--- src/botPage/bot/NewTradeEngine/sagas/handleProposalSubscription/index.spec.js
+++ src/botPage/bot/NewTradeEngine/sagas/handleProposalSubscription/index.spec.js
@@ -1,10 +1,20 @@
+import { eventChannel } from 'redux-saga';
+import { testSaga } from 'redux-saga-test-plan';
+import dataStream from '../dataStream';
+import * as selectors from '../selectors';
+import requestProposals from './requestProposals';
+import handleProposalReady from './handleProposalReady';
+import handleProposalStream from './handleProposalStream';
+import handleForgottenProposal from './handleForgottenProposal';
 import handleProposalSubscription from './';
 
+const fakeChannel = eventChannel(() => () => {});
+const tradeOption = {};
 const $scope = {};
 const proposalID1 = 'proposalID1';
 const proposalID2 = 'proposalID2';
-const proposal1 = { id: '0' };
-const proposal2 = { id: '1' };
+const proposal1 = { id: '0', uuid: proposalID1 };
+const proposal2 = { id: '1', uuid: proposalID2 };
 const payload = { [proposalID1]: proposal1, [proposalID2]: proposal2 };
 
 describe('handleProposalSubscription', () => {
@@ -13,9 +23,9 @@ describe('handleProposalSubscription', () => {
             .next()
             .select(selectors.receivedProposals)
             .next(payload)
-            .fork(handleForgottenProposal, { $scope, proposalID: proposalID1 })
+            .fork(handleForgottenProposal, { $scope, proposal: proposal1 })
             .next()
-            .fork(handleForgottenProposal, { $scope, proposalID: proposalID2 })
+            .fork(handleForgottenProposal, { $scope, proposal: proposal2 })
             .next()
             .fork(requestProposals, tradeOption)
             .next()
