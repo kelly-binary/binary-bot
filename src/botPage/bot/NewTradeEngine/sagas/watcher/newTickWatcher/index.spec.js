8a3df6a6d2772c15ecc6caa594b3c8ea63179ebb
kelly <kelly@binary.com>
2017-06-05 17:24:12 +0800

    Add test for new tick watcher

--- src/botPage/bot/NewTradeEngine/sagas/watcher/newTickWatcher/index.spec.js
+++ src/botPage/bot/NewTradeEngine/sagas/watcher/newTickWatcher/index.spec.js
@@ -1,5 +1,37 @@
+import { testSaga } from 'redux-saga-test-plan';
+import * as actions from '../../../constants/actions';
+import * as states from '../../../constants/states';
+import * as selectors from '../../selectors';
+import newTickWatcher from './';
+
 describe('newTickWatcher saga', () => {
-    it('should EXIT_BEFORE_PURCHASE_SCOPE if stage is not STARTED or PROPSOALS_READY', () => {});
-    it('should ENTER_BEFORE_PURCHASE_SCOPE if stage is PROPOSALS_READY', () => {});
-    it('should ENTER_BEFORE_PURCHASE_SCOPE after RECEIVE_ALL_PROPOSALS if stage is STARTED', () => {});
+    it('should EXIT_BEFORE_PURCHASE_SCOPE if stage is not STARTED or PROPSOALS_READY', () => {
+        testSaga(newTickWatcher)
+            .next()
+            .select(selectors.stage)
+            .next(states.STOPPED)
+            .put({ type: actions.EXIT_BEFORE_PURCHASE_SCOPE })
+            .next()
+            .isDone();
+    });
+    it('should ENTER_BEFORE_PURCHASE_SCOPE if stage is PROPOSALS_READY', () => {
+        testSaga(newTickWatcher)
+            .next()
+            .select(selectors.stage)
+            .next(states.PROPOSALS_READY)
+            .put({ type: actions.ENTER_BEFORE_PURCHASE_SCOPE })
+            .next()
+            .isDone();
+    });
+    it('should ENTER_BEFORE_PURCHASE_SCOPE after RECEIVE_ALL_PROPOSALS if stage is STARTED', () => {
+        testSaga(newTickWatcher)
+            .next()
+            .select(selectors.stage)
+            .next(states.STARTED)
+            .take(actions.RECEIVE_ALL_PROPOSALS)
+            .next()
+            .put({ type: actions.ENTER_BEFORE_PURCHASE_SCOPE })
+            .next()
+            .isDone();
+    });
 });
