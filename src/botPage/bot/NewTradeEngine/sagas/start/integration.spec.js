0abe8b7f87b42dfd16cae36723f7436e7407c6ea
Amin Marashi <marashi.s.amin@gmail.com>
2017-06-05 14:09:17 +0800

    Remove integration test for start

--- src/botPage/bot/NewTradeEngine/sagas/start/integration.spec.js
+++
@@ -1,9 +0,0 @@
-describe('start and proposal subscription integration', () => {
-    expect()
-        .put({ type: `UPDATE_${actions.RECEIVED_PROPOSAL}` })
-        .put({ type: `UPDATE_${actions.RECEIVED_PROPOSAL}` })
-        .put({ type: `UPDATE_${actions.REQUESTED_PROPOSAL}` })
-        .put({ type: `UPDATE_${actions.REQUESTED_PROPOSAL}` })
-        .put({ type: `UPDATE_${actions.REQUESTED_PROPOSAL}` })
-        .put({ type: `UPDATE_${actions.REQUESTED_PROPOSAL}` });
-});
