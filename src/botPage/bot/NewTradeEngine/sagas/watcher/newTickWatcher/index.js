5b0c13d0e301ce888d802d4d42b9d0c08e5b501a
Amin Marashi <marashi.s.amin@gmail.com>
2017-06-05 17:28:00 +0800

    Add code for newTickWatcher

--- src/botPage/bot/NewTradeEngine/sagas/watcher/newTickWatcher/index.js
+++ src/botPage/bot/NewTradeEngine/sagas/watcher/newTickWatcher/index.js
@@ -1,3 +1,16 @@
+import { select, put, take } from 'redux-saga/effects';
+import * as actions from '../../../constants/actions';
+import * as states from '../../../constants/states';
+import * as selectors from '../../selectors';
+
 export default function* newTickWatcher() {
-    yield 0;
+    const stage = yield select(selectors.stage);
+    if (stage === states.PROPOSALS_READY) {
+        yield put({ type: actions.ENTER_BEFORE_PURCHASE_SCOPE });
+    } else if (stage === states.STARTED) {
+        yield take(actions.RECEIVE_ALL_PROPOSALS);
+        yield put({ type: actions.ENTER_BEFORE_PURCHASE_SCOPE });
+    } else {
+        yield put({ type: actions.EXIT_BEFORE_PURCHASE_SCOPE });
+    }
 }
