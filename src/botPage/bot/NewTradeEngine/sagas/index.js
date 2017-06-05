746cd78a5eddac7e68bd208d46ab0dec44d468d5
Amin Marashi <marashi.s.amin@gmail.com>
2017-06-05 16:31:44 +0800

    Add takeEvery for actions to trigger watches

--- src/botPage/bot/NewTradeEngine/sagas/index.js
+++ src/botPage/bot/NewTradeEngine/sagas/index.js
@@ -4,11 +4,14 @@ import customTakeEvery from './customTakeEvery';
 import error from './error';
 import init from './init';
 import start from './start';
-import watch from './watch';
+import waitForPurchase from './watch/waitForPurchase';
+import waitForSell from './watch/waitForSell';
 
 export default function* rootSaga() {
     yield takeEvery('*', error);
     yield customTakeEvery(actions.INIT_SAGA, init);
     yield customTakeEvery(actions.START_SAGA, start);
-    yield customTakeEvery(actions.WATCH_SAGA, watch);
+    yield customTakeEvery(actions.NEW_TICK, waitForPurchase);
+    yield customTakeEvery(actions.RECEIVE_ALL_PROPOSALS, waitForPurchase);
+    yield customTakeEvery(actions.OPEN_CONTRACT, waitForSell);
 }
