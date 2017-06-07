bbfde8933f3a1348a47129a744a1ae44c25749b4
Amin Marashi <marashi.s.amin@gmail.com>
2017-06-07 15:59:44 +0800

    Add purchase actions to purchase saga test

--- src/botPage/bot/NewTradeEngine/sagas/purchase/index.spec.js
+++ src/botPage/bot/NewTradeEngine/sagas/purchase/index.spec.js
@@ -1,5 +1,6 @@
 import { testSaga } from 'redux-saga-test-plan';
 import * as selectors from '../selectors';
+import * as actions from '../../constants/actions';
 import requestPurchase from './requestPurchase';
 import purchase from './';
 
@@ -14,14 +15,31 @@ const receivedProposals = {
     uuid2: selectedProposal,
 };
 
+const error = Error('Unsuccesssful Purchase');
+
 describe('Purchase saga', () => {
     it('should select a proposal using the contract type and purchase it', () => {
         testSaga(purchase, { $scope, contractType })
             .next()
             .select(selectors.receivedProposals)
             .next(receivedProposals)
+            .put({ type: actions.REQUEST_PURCHASE })
+            .next()
             .call(requestPurchase, { $scope, proposal: selectedProposal })
             .next()
+            .put({ type: actions.PURCHASE_SUCCESSFULLY })
+            .isDone();
+    });
+    it('should put PURCHASE_UNSUCCESSFULLY if requestPurchase fails', () => {
+        testSaga(purchase, { $scope, contractType })
+            .next()
+            .select(selectors.receivedProposals)
+            .next(receivedProposals)
+            .put({ type: actions.REQUEST_PURCHASE })
+            .next()
+            .call(requestPurchase, { $scope, proposal: selectedProposal })
+            .throw(error)
+            .put({ type: actions.PURCHASE_UNSUCCESSFULLY, payload: error, error: true })
             .isDone();
     });
 });
