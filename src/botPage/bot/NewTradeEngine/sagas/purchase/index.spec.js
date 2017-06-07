8006fde1dd098a82f44d08f2846df80d44734f2a
Amin Marashi <marashi.s.amin@gmail.com>
2017-06-07 15:44:42 +0800

    Add requestPurchase to purchase saga test

--- src/botPage/bot/NewTradeEngine/sagas/purchase/index.spec.js
+++ src/botPage/bot/NewTradeEngine/sagas/purchase/index.spec.js
@@ -1,5 +1,25 @@
+import { testSaga } from 'redux-saga-test-plan';
+import * as selectors from '../selectors';
+import requestPurchase from './requestPurchase';
+import purchase from './';
+
+const contractType = 'CALL';
+
+const selectedProposal = { contract_type: 'CALL' };
+
+const receivedProposals = {
+    uuid1: {},
+    uuid2: selectedProposal,
+};
+
 describe('Purchase saga', () => {
     it('should select a proposal using the contract type and purchase it', () => {
-        testSaga(purchase, contractType).next().select(selectors.receivedProposals).next(selectedProposal).isDone();
+        testSaga(purchase, contractType)
+            .next()
+            .select(selectors.receivedProposals)
+            .next(receivedProposals)
+            .call(requestPurchase, selectedProposal)
+            .next()
+            .isDone();
     });
 });
