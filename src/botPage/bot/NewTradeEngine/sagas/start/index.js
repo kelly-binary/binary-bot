8464135c549b20ec1179c436fe0b7d33a154c6d7
Amin Marashi <marashi.s.amin@gmail.com>
2017-06-08 15:34:53 +0800

    Move isSameTradeOption to tools

--- src/botPage/bot/NewTradeEngine/sagas/start/index.js
+++ src/botPage/bot/NewTradeEngine/sagas/start/index.js
@@ -1,34 +1,9 @@
 import { select, put, spawn } from 'redux-saga/effects';
+import { isTradeOptionTheSame } from '../../../tools';
 import * as actions from '../../constants/actions';
 import * as states from '../../constants/states';
 import * as selectors from '../selectors';
 import proposal from '../proposal';
-
-const isTradeOptionTheSame = (oldOpt, newOpt) =>
-    [
-        'contractTypes',
-        'symbol',
-        'duration',
-        'duration_unit',
-        'amount',
-        'currency',
-        'prediction',
-        'barrierOffset',
-        'secondBarrierOffset',
-    ].every(field => {
-        if (oldOpt[field] === newOpt[field]) {
-            return true;
-        } else if (field === 'contractTypes') {
-            try {
-                const [oldType1, oldType2] = oldOpt[field];
-                const [type1, type2] = newOpt[field];
-                return type1 === oldType1 && type2 === oldType2;
-            } catch (e) {
-                return false;
-            }
-        }
-        return false;
-    });
 
 export default function* start(arg) {
     const { tradeOption } = arg;
--- src/botPage/bot/tools.js
+++ src/botPage/bot/tools.js
@@ -166,3 +166,29 @@ export const createDetails = contract => {
         result,
     ];
 };
+
+export const isTradeOptionTheSame = (oldOpt, newOpt) =>
+    [
+        'contractTypes',
+        'symbol',
+        'duration',
+        'duration_unit',
+        'amount',
+        'currency',
+        'prediction',
+        'barrierOffset',
+        'secondBarrierOffset',
+    ].every(field => {
+        if (oldOpt[field] === newOpt[field]) {
+            return true;
+        } else if (field === 'contractTypes') {
+            try {
+                const [oldType1, oldType2] = oldOpt[field];
+                const [type1, type2] = newOpt[field];
+                return type1 === oldType1 && type2 === oldType2;
+            } catch (e) {
+                return false;
+            }
+        }
+        return false;
+    });
