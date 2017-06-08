65fbb632c9a10bcbddd7c0fe37bd77509ccf6969
Amin Marashi <marashi.s.amin@gmail.com>
2017-06-08 11:15:24 +0800

    Add requestProposalSubscription

---
+++ src/botPage/bot/NewTradeEngine/sagas/requestPropsoalSubscription/index.spec.js
@@ -0,0 +1,24 @@
+import { testSaga } from 'redux-saga-test-plan';
+import { tradeOptionToProposal } from '../../../tools';
+import handleProposalSubscription from '../start/handleProposalSubscription';
+import requestProposalSubscription from './';
+
+const tradeOption = {
+    contractTypes: ['PUT', 'CALL'],
+    amount       : 12.00,
+};
+
+const $scope = {};
+
+const proposalRequests = {};
+
+describe('requestProposalSubscription saga', () => {
+    it('should call proposal subscription saga with given tradeOption', () => {
+        testSaga(requestProposalSubscription, { $scope, tradeOption })
+            .call(tradeOptionToProposal, tradeOption)
+            .next(proposalRequests)
+            .call(handleProposalSubscription, { $scope, proposalRequests })
+            .next()
+            .isDone();
+    });
+});
