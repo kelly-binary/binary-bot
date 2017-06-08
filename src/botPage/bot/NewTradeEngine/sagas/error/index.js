219cab553de72c1d5c9c425f210c6c2c0b026797
Amin Marashi <marashi.s.amin@gmail.com>
2017-06-08 17:22:33 +0800

    Fix error saga to use FSA type error action

--- src/botPage/bot/NewTradeEngine/sagas/error/index.js
+++ src/botPage/bot/NewTradeEngine/sagas/error/index.js
@@ -2,9 +2,9 @@ import { observer as globalObserver } from 'binary-common-utils/lib/observer';
 import { call, put } from 'redux-saga/effects';
 import * as actions from '../../constants/actions';
 
-export default function* error({ type, payload }) {
-    if (type.match(/.*_ERROR/)) {
+export default function* errorSaga({ payload, error }) {
+    if (error) {
         yield call([globalObserver, globalObserver.emit], 'Error', payload);
-        yield put({ type: actions.ERROR_OCCURRED, error: true });
+        yield put({ type: actions.ERROR_OCCURRED, payload, error: true });
     }
 }
--- src/botPage/bot/NewTradeEngine/sagas/error/index.spec.js
+++ src/botPage/bot/NewTradeEngine/sagas/error/index.spec.js
@@ -1,20 +1,21 @@
 import { observer as globalObserver } from 'binary-common-utils/lib/observer';
 import { testSaga } from 'redux-saga-test-plan';
 import * as actions from '../../constants/actions';
-import error from './';
+import errorSaga from './';
+
+const payload = Error('some error');
 
 describe('error saga', () => {
-    const payload = Error('some error');
     it('should call globalObserver with the error', () => {
-        testSaga(error, { type: 'SOMETHING_ERROR', payload })
+        testSaga(errorSaga, { type: 'SOMETHING', payload })
             .next()
             .call([globalObserver, globalObserver.emit], 'Error', payload)
             .next()
-            .put({ type: actions.ERROR_OCCURRED, error: true })
+            .put({ type: actions.ERROR_OCCURRED, payload, error: true })
             .next()
             .isDone();
     });
     it('should do nothing in case of non-error type', () => {
-        testSaga(error, { type: 'SOMETHING' }).next().isDone();
+        testSaga(errorSaga, { type: 'SOMETHING' }).next().isDone();
     });
 });
