fc22a2d1e3076ada325c0e95e5c275bd6cc43479
Amin Marashi <marashi.s.amin@gmail.com>
2017-06-09 13:45:48 +0800

    Move proposal constants to properties constants

--- src/botPage/bot/NewTradeEngine/reducers/proposalInfo.js
+++ src/botPage/bot/NewTradeEngine/reducers/proposalInfo.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
-import * as actions from '../constants/actions';
+import * as properties from '../constants/properties';
 import objectReducerCreator from './reducerCreators/object';
 
-const requestedProposals = objectReducerCreator(actions.REQUESTED_PROPOSAL);
-const receivedProposals = objectReducerCreator(actions.RECEIVED_PROPOSAL);
-const forgottenProposals = objectReducerCreator(actions.FORGOTTEN_PROPOSAL);
+const requestedProposals = objectReducerCreator(properties.REQUESTED_PROPOSAL);
+const receivedProposals = objectReducerCreator(properties.RECEIVED_PROPOSAL);
+const forgottenProposals = objectReducerCreator(properties.FORGOTTEN_PROPOSAL);
 
 export default combineReducers({
     requestedProposals,
