6c5a06f206b911467601d1cde47dde3c857aec46
Amin Marashi <marashi.s.amin@gmail.com>
2017-06-09 14:38:33 +0800

    Fix updateWaitingForPurchase

--- src/botPage/bot/NewTradeEngine/actions/updateWaitingForPurchase/index.js
+++ src/botPage/bot/NewTradeEngine/actions/updateWaitingForPurchase/index.js
@@ -1,16 +1,16 @@
 import { translate } from '../../../../../common/i18n';
-import * as actions from '../../constants/actions';
+import * as properties from '../../constants/properties';
 
 export default function({ timestamp, stayInsideScope, error }) {
     if (error) {
         return {
-            type   : actions.UPDATE_WAITING_FOR_PURCHASE,
+            type   : `UPDATE_${properties.WAITING_FOR_PURCHASE}`,
             payload: Error(translate('Bot should be started before calling watch function')),
             error  : true,
         };
     }
     return {
-        type   : actions.UPDATE_WAITING_FOR_PURCHASE,
+        type   : `UPDATE_${properties.WAITING_FOR_PURCHASE}`,
         payload: { timestamp, stayInsideScope },
     };
 }
--- src/botPage/bot/NewTradeEngine/actions/updateWaitingForPurchase/index.spec.js
+++ src/botPage/bot/NewTradeEngine/actions/updateWaitingForPurchase/index.spec.js
@@ -1,5 +1,5 @@
 import { translate } from '../../../../../common/i18n';
-import * as actions from '../../constants/actions';
+import * as properties from '../../constants/properties';
 import updateWaitingForPurchase from './';
 
 const timestamp = 123456;
@@ -8,7 +8,7 @@ const stayInsideScope = true;
 describe('throw waitingForPurchase action creator', () => {
     it('should return an error action', () => {
         expect(updateWaitingForPurchase({ error: true })).toEqual({
-            type   : actions.UPDATE_WAITING_FOR_PURCHASE,
+            type   : `UPDATE_${properties.WAITING_FOR_PURCHASE}`,
             payload: Error(translate('Bot should be started before calling watch function')),
             error  : true,
         });
@@ -17,7 +17,7 @@ describe('throw waitingForPurchase action creator', () => {
 describe('updateWaitingForPurchase action creator', () => {
     it('should return an action made of given timestamp and stayInsideScope flag', () => {
         expect(updateWaitingForPurchase({ timestamp, stayInsideScope })).toEqual({
-            type   : actions.UPDATE_WAITING_FOR_PURCHASE,
+            type   : `UPDATE_${properties.WAITING_FOR_PURCHASE}`,
             payload: { timestamp, stayInsideScope },
         });
     });
